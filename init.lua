021f12007beaae3a6b2b3f0f3d7dda9900aed187
Arya <pranishstx@gmail.com>
2023-08-29 17:23:08 -0400

    Simple edits + new Keymap

---
+++ init.lua
@@ -0,0 +1,10 @@
+vim.opt.relativenumber = true
+vim.opt.mouse = 'a'
+vim.opt.hlsearch = false
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
+vim.keymap.set('n','<space>w','<cmd>write<CR>', {desc='Save'})
+
